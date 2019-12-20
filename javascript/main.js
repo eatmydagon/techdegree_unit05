c7de73f1662c1fc52cd23547b382284af616c68f
Jess D'Souza <change.the.world.jess@hotmail.com>
2019-12-21 01:44:48 +0400

    changed search method in js

--- javascript/main.js
+++ javascript/main.js
@@ -12,7 +12,7 @@ const $gallery_items = $(".gallery a");
 
 $("#search").on("keyup", function(event) {
   let $search = $(event.target).val().toUpperCase();
-  for ( let i = 0 ; i < 12 ; i += 1) {
+  for ( let i = 0 ; i < $gallery_items.length ; i += 1) {
     let $search_item = $gallery_items.eq(i);
     if ($search_item.attr("title").toUpperCase().indexOf($search) === -1) {
       $search_item.fadeOut(500);
